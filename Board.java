a77897c789970ef49598347d7108933650416540
zhouand25 <zhouand25@7hills.org>
2023-10-04 13:04:41 -0400

    Random gen start

--- Board.java
+++ Board.java
@@ -27,13 +27,31 @@ public class Board {
 
     public void generate() {
         for(int move=1; move<numShips+1; ++move) {
-            ships[move-1]
+            ships[move-1] = 
 
         }
     }
 
     private int[] iterate(int size, int direc) {
+        if(direc==1 || direc == 2) {
 
+            
+        }
     }
 
+    /* random stuff start
+    randPos(m1, m2);
+    randPos(m2, m1);
+    function randPos(int x, int y) {
+        int seedX = Math.floor(Math.random()*(cols-x+1));
+        int seedY = Math.floor(Math.random()*(rows-y+1));board[i][j]
+        
+        for(int i=seedX; i<seedX+x; ++i) {board[i][j]
+            for(int j=seedY; j<seedY+y; ++j) {board[i][j]
+                board[i][j]
+            }
+
+        }
+    }
+    */
 }
