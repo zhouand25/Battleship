f89ef9e3606db9211d9f66cfe42fb5fccf7d2290
zhouand25 <124077648+zhouand25@users.noreply.github.com>
2023-10-20 10:58:39 -0400

    Starting Manuel set

--- Tester.java
+++ Tester.java
@@ -1,11 +1,7 @@
 public class Tester {
-    
-    public static void main(String[] args) {
-        //g1 is player board
-        Board g1 = new Board();
-        //g2 is the computer's board
-        Board g2 = new Board();
-
-    }
-
+public static void main(String[] args) {
+     Board myBoard = new Board();
+     Board computer = new Board();
+     myBoard.manualSet();
+}
 }
